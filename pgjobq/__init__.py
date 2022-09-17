9ebd4f059ce306ef051ef5d0ba7d29abb5bbb203
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-09-17 12:12:12 -0500

    Initial commit

--- pgjobq/__init__.py
+++ pgjobq/__init__.py
@@ -1,7 +1,6 @@
-from pgjobq._queue import connect_to_queue, Send, Receive
-from pgjobq._migrations import migrate_to_latest_version
 from pgjobq._crud import create_queue, delete_queue
-
+from pgjobq._migrations import migrate_to_latest_version
+from pgjobq._queue import Receive, Send, connect_to_queue
 
 __all__ = (
     "connect_to_queue",
